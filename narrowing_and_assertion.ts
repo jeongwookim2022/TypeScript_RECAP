adb100458df57deccac94cb3907c993894dae145
jeongwookim2022 <jeongwookim2023@gmail.com>
2024-01-09 23:15:36 +0100

    Narrowing and Assertion

---
+++ narrowing_and_assertion.ts
@@ -0,0 +1,35 @@
+// Q1
+function clean(dirtyArr: (string | number)[]): number[] {
+  let cleanArr: number[] = [];
+
+  dirtyArr.forEach((el) => {
+    if (typeof el === "string") {
+      cleanArr.push(parseFloat(el));
+    } else {
+      cleanArr.push(el);
+    }
+  });
+  // for (let i = 0; i < dirtyArr.length; i++) {
+  //   if (typeof dirtyArr[i] === "string") {
+  //     cleanArr.push(parseInt(dirtyArr[i]));
+  //   } else if (typeof dirtyArr[i] === "number") {
+  //     cleanArr.push(dirtyArr[i] as number);
+  //   }
+  // }
+  return cleanArr;
+}
+
+// console.log(clean([1, 2, "3", 4, "11"]));
+
+// Q2
+function school(teacher: { subject: string[] | string }) {
+  if (typeof teacher.subject === "string") {
+    return teacher.subject;
+  } else if (Array.isArray(teacher.subject)) {
+    return teacher.subject[teacher.subject.length - 1];
+  } else {
+    return "Prop TYPE ERROR";
+  }
+}
+
+console.log(school({ subject: ["science", "art", "korean"] }));
